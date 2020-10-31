6cebd9be4c995a6bb23cb36f84c392f7ac2275a0
AnelZhaksybergenova <anelbolatbek1@gmail.com>
2020-10-31 11:42:32 -0500

    new lines

--- src/main/java/HalloweenTest.java
+++ src/main/java/HalloweenTest.java
@@ -2,6 +2,10 @@ public class HalloweenTest {
     public static void main(String[] args) {
         System.out.println("another one");
         System.out.println("new new new");
+        //new one
+        //this new line was made
+        //more new line
+        //
         //
     }
 }
